2e190cea1570aa602c1fbc8e71e8106b30b705c5
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2026-05-20 21:28:37 +0200

    prevent round timer overflow

--- shared/constants.ts
+++ shared/constants.ts
@@ -15,10 +15,12 @@ export enum Joker {
   SPY = 'SPY'
 }
 
-export const MAX_VOLUME = 0.25;
-export const DEFAULT_VOLUME_SLIDER_VAL = 0.5;
-export const POLLING_INTERVAL = 500;
-export const DEFAULT_TRACK_DURATION = 60000;
-export const DEFAULT_ROUNDS = 5;
-export const BASE_POINTS = 100;
-export const FIRST_BONUS_MULTIPLIER = 1.2;
+export const MAX_VOLUME: number = 0.25;
+export const DEFAULT_VOLUME_SLIDER_VAL: number = 0.5;
+export const POLLING_INTERVAL: number = 500;
+export const DEFAULT_TRACK_DURATION: number = 60000;
+export const DEFAULT_ROUNDS: number = 5;
+export const BASE_POINTS: number = 100;
+export const FIRST_BONUS_MULTIPLIER: number = 1.2;
+export const INT32_MAX_VALUE: number = 2**31 - 1;
+export const COUNTDOWN_DURATION: number = 4000;
